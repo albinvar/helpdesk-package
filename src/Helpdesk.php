5a2b8ca699d832990773677dc81c4acebcbc5ff9
albinvar <albinvar@users.noreply.github.com>
2021-08-12 10:02:32 +0000

    Fix styling

--- src/Helpdesk.php
+++ src/Helpdesk.php
@@ -2,33 +2,31 @@
 
 namespace Albinvar\Helpdesk;
 
-use Albinvar\Helpdesk\Models\HelpdeskTicket;
 use Albinvar\Helpdesk\Models\HelpdeskDepartment;
 
 class Helpdesk
 {
-	
-	protected $collection = null;
-	
-	protected $type;
-	
+    protected $collection = null;
+
+    protected $type;
+
     public function __construct()
     {
         //
     }
 
-    public function department($id=null)
+    public function department($id = null)
     {
-    	$this->type = 'department';
-    
-    	(is_null($id)) ? $this->collection = HelpdeskDepartment::all()
-						: $this->collection = HelpdeskDepartment::find($id);
-    	
+        $this->type = 'department';
+
+        (is_null($id)) ? $this->collection = HelpdeskDepartment::all()
+                        : $this->collection = HelpdeskDepartment::find($id);
+
         return $this;
     }
-    
+
     public function get()
     {
-		return $this->collection;
+        return $this->collection;
     }
 }
