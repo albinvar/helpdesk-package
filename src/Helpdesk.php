66a6f124aec843aa64d58b7446cb042f33bdd0c3
Albin Varghese <albin8285@gmail.com>
2021-08-12 15:12:31 +0530

    get all departments

--- src/Helpdesk.php
+++ src/Helpdesk.php
@@ -3,16 +3,35 @@
 namespace Albinvar\Helpdesk;
 
 use Albinvar\Helpdesk\Models\HelpdeskTicket;
+use Albinvar\Helpdesk\Models\HelpdeskDepartment;
 
 class Helpdesk
 {
+	
+	protected $collection = null;
+	
+	protected $type;
+	
     public function __construct()
     {
         //
     }
 
-    public function getTickets()
+    public function department($id=null)
     {
-        return HelpdeskTicket::all();
+    	if(is_null($id))
+	    {
+			$this->collection = HelpdeskDepartment::all();
+		} else {
+			$this->collection = HelpdeskDepartment::find($id);
+		}
+    	
+    	$this->type = 'department';
+        return $this;
+    }
+    
+    public function get()
+    {
+		return $this->collection;
     }
 }
