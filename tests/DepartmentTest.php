9ab6018d88f16c412e1b7f0036741b0182999d91
Albin Varghese <albin8285@gmail.com>
2021-08-13 13:11:40 +0530

    test for checkimg if get method works

--- tests/DepartmentTest.php
+++ tests/DepartmentTest.php
@@ -4,14 +4,30 @@ namespace Albinvar\Helpdesk\Tests;
 
 use Albinvar\Helpdesk\Helpdesk;
 use Illuminate\Database\Eloquent\Collection;
+use Albinvar\Helpdesk\Models\HelpdeskDepartment;
 
 class DepartmentTest extends TestCase
 {
+	/**
+	check if all() method works.
+	**/
     public function test_if_deparment_all_method_works()
     {
-    	$departments = new Helpdesk();
-		$departments->department()->all();
+    	$helpdesk = new Helpdesk();
+	    $departments = $helpdesk->department()->all();
+	    $this->assertInstanceOf(Collection::class , $departments);
+		$this->assertEquals(HelpdeskDepartment::all(), $departments);
+    }
     
-	    $this->assertInstanceOf(Collection::class , $departments->department()->all());
+    /**
+    check if the collection extracted from get method mathches eloquent model.
+    **/
+    public function test_if_deparment_get_method_works()
+    {
+    	$department = HelpdeskDepartment::factory()->create();
+    
+    	$helpdesk = new Helpdesk();
+	
+	    $this->assertEquals(HelpdeskDepartment::find($department->id), $helpdesk->department($department->id)->get());
     }
 }
