756b154ad8a3ccaada76b6465cd1f6aa6d4adcb2
Albin Varghese <albin8285@gmail.com>
2021-08-13 11:02:12 +0530

    add all() to fetch all records

--- src/Helpdesk.php
+++ src/Helpdesk.php
@@ -19,9 +19,9 @@ class Helpdesk
 
     public function department($id=null)
     {
-    	$this->type = 'department';
+    	$this->type = __FUNCTION__;
     
-    	(is_null($id)) ? $this->collection = HelpdeskDepartment::all()
+    	(is_null($id)) ? $this->collection = null
 						: $this->collection = HelpdeskDepartment::find($id);
     	
         return $this;
@@ -30,5 +30,10 @@ class Helpdesk
     public function get()
     {
 		return $this->collection;
+    }
+    
+    public function all()
+    {
+		return $this->collection = HelpdeskDepartment::all();
     }
 }
